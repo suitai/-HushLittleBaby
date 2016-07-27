1dfab3d12526d7a06dcd2a93a6f5b719e155744c
suitai <suitai@outlook.jp>
2016-07-28 01:55:07 +0900

    関数を分割

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,36 @@
 $(function() {
     $("input[name='twtype']").change(function() {
-        $('.content').html("");
-        var data = {twtype: $("input[name='twtype']:checked").val()};
-        $.ajax({
-            url: '/_get_tweets',
-            type: 'get',
-            data: data,
-            dataType: 'html',
-        }).done(function(result) {
-            $('.content').html(result);
-        }).fail(function(result) {
-            $('.content').html(result);
-            console.log("ERROR: ");
+        show_tweets({
+            twtype: $(":input[name='twtype']:checked").val(),
+            params: {count: 100}
+        });
+    });
+
         });
     });
 });
+
+function show_tweets(data){
+    console.log("show_tweets");
+    console.log(data);
+    $('.content').html("");
+
+    get_tweets(data).done(function(result) {
+        $('.content').html(result);
+        console.log("get_tweets")
+    }).fail(function(result) {
+        $('.content').html("<p>" + result.statusText + "</p>");
+        console.log("error");
+        console.log(result);
+    });
+}
+
+function get_tweets(data){
+    return $.ajax({
+        url: '_get_tweets',
+        type: 'post',
+        data: JSON.stringify(data),
+        contentType: 'application/json',
+        dataType: 'html',
+    });
+}
